b049daf334ae18b7b76f079d6a0f15809a758f78
Sergey-Barmaley-1 <151543029+Sergey-Barmaley-1@users.noreply.github.com>
2025-06-28 14:02:24 +0300

    install react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
-        <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
