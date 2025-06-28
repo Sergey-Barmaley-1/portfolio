475a75deb674d0c2dc9646c6c4839467985b5057
Sergey-Barmaley-1 <151543029+Sergey-Barmaley-1@users.noreply.github.com>
2025-06-28 14:15:44 +0300

    add Pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFoundPage from './pages/NotFoundPage';
+import Contacts from './pages/Contacts';
+import Projects from './pages/Projects';
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
